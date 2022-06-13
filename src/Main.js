23df26f5dc95b554f052b122ffb8ce1951e7228c
sheyna <sheynawatkins@gmail.com>
2022-06-13 12:28:27 -0700

    set up person components.

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Person from './Person.js';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <Person name="Sheyna"/>
+        <Person name="Brentice"/>
+        <Person name="Yari"/>
+        <Person name="Kris"/>
+        <Person name="Matthew"/>
+        <Person name="Jamall"/>
+      </main>
+    )
+  }
+}
+
+export default Main;
